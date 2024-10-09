2095293a2c0cbef19fb6d4c486af5e6c86dd859d
Sergio Morales <chemoral87@hotmail.com>
2024-10-09 16:12:29 -0600

    eocsystem port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,11 @@ module.exports = {
       exec_mode: "cluster",
       instances: "max", // Or a number of instances
       script: "./node_modules/nuxt/bin/nuxt.js",
-      args: "start"
+      args: "start",
+      env: {
+        PORT: 3000, // Set the desired port
+        NODE_ENV: "production"
+      }
     }
   ]
 };
